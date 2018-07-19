5554f60315a54fdbd5922d6fbcb67696f737b25e
Martin Jaycy Halum <>
2018-07-19 09:38:36 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import store from './app/store'; //Import the store
-import Home from './app/components/home' //Import the component file
-
-export default class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <Home />
-            </Provider>
-        );
-    }
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
