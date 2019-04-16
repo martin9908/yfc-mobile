ccdee07282daefc179b6d633c484b8777479b10d
Martin Jaycy Halum <>
2019-04-16 12:19:35 +0800

    fcm push notif fix

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp7a4453af67be412bbdf6cf4bf024a545://10.1.3.156:19000";
+  public static final String DEVELOPMENT_URL = "exp7a4453af67be412bbdf6cf4bf024a545://192.168.1.7:19000";
 
 }
